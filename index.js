ce522a3c2a7dda185174484ecfbb988c695c4754
Roy <loganroyjr4@gmail.com>
2022-09-30 19:43:03 -0500

    added .then promise to initial user choice... used switch () to break out each case w/ respective functions called out

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ inquirer
             choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update employee role'] 
         }
     ])
+    .then((answers) => {
+        switch (answers.userChoice) {
+            case "view all departments":
+                viewAllDepartments();
+                break;
+            case "view all roles":
+                viewAllRoles();
+                break;
+            case "view all employees":
+                viewAllEmployees();
+                break;
+            case "add a department" :
+                addADepartment();
+                break;
+            case "add a role" :
+                addARole();
+                break; 
+            case "add an employee" :
+                addAnEmployee();
+                break;
+            case "update employee role" :
+                updateEmployeeRole();
+                break;              
+        }
+    })
