614aa07ba05774a5e354f07750c7aeb4a4299068
Roy <loganroyjr4@gmail.com>
2022-09-30 20:43:45 -0500

    added code to schema.sql

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const inquirer = require('inquirer')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: 'rootroot', 
+})
 
 inquirer
     .prompt([
